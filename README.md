d1b497179141bdb9b0e717008c1d623ddc52ca90
sourav kapahu <kumarsourav413@gmail.com>
2025-06-23 00:10:08 +0530

    Setup and New modules added.

--- README.md
+++
@@ -1,2 +0,0 @@
-# React_Portfolio
-A modern, responsive developer portfolio built with React.js showcasing my projects, skills, and experience. Includes reusable components, smooth animations, and mobile-first design.
---
+++ src/routes/AppRoutes.tsx
@@ -0,0 +1,16 @@
+import { Routes, Route, Outlet } from 'react-router-dom'
+import Home from '../pages/home'
+import About from '../pages/about'
+import MainLayout from '../layouts/main'
+
+const AppRoutes = () => (
+    <Routes>
+        <Route element={<MainLayout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+        </Route>
+
+    </Routes>
+)
+
+export default AppRoutes
